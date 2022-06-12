95ac64051bc9b05c047c479bbaa54983d0b2ae9f
KAROLINA LUSZCZ <karolina.luszcz@sojournsolutions.com>
2022-06-12 22:43:39 +0200

    router

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter, Routes,  Route} from "react-router-dom";
-import TestingRoute from "./routes/TestingRoute";
+
+import Router from "./routes/Router";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="" element={<App />} />
-      <Route path="testingRoute" element={<TestingRoute />} />
-    </Routes>
-    </BrowserRouter>
+    <Router />
   </React.StrictMode>
 )
--- src/routes/TestingRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function TestingRoute() {
-    return (
-        <Link to="/">Main</Link>
-    );
-  }
